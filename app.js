8acd5ff5fcb5401c6404c30883460e9c69b971f7
Artur <semenukartur1@gmail.com>
2022-12-14 00:18:20 +0200

    add model and slide

--- app.js
+++ app.js
@@ -22,3 +22,31 @@ addToCartBtns.forEach((item) =>
     productsCountEt.textContent = +productsCountEt.textContent + 1;
   })
 );
+
+let moreDetailsBtn = document.querySelectorAll(".more-deteils");
+let model = document.querySelector(".model");
+let btnClose = document.querySelector(".btn-close");
+
+moreDetailsBtn.forEach((item) => item.addEventListener("click", openMoodel));
+
+btnClose.addEventListener("click", closeModel);
+
+function openMoodel() {
+  model.classList.add("show");
+  model.classList.remove("hide");
+}
+
+function closeModel() {
+  model.classList.add("hide");
+  model.classList.remove("show");
+}
+
+model.addEventListener("click", function (e) {
+  console.log(e.target);
+  if (e.target === model) {
+    closeModel();
+  }
+});
+
+// slider
+$(".slaider-block").slick();
